5b1516fba8aa19d84ef7da0f500df9409f01a545
Abhirup60 <abhirupgit36@gmail.com>
2024-10-21 16:12:34 +0530

    update

--- my-app/src/modules/CreatePost/Createpost.js
+++ my-app/src/modules/CreatePost/Createpost.js
@@ -37,7 +37,7 @@ const Createpost = () => {
             const { secure_url } = await uploadImage();
             seturl(secure_url);
         
-            const response = await fetch('http://localhost:8000/api/new-post', {
+            const response = await fetch('https://social-media-project-three.vercel.app/api/new-post', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
