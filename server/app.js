44a7e473fac343db130b40a2ec11405037b78be9
Abhirup60 <abhirupgit36@gmail.com>
2024-10-21 15:45:53 +0530

    add vercel.josn and update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,13 @@ const authenticate = require('./middleware/auth');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://social-media-project-frontend.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 
 const port = process.env.PORT || 8000;
 
